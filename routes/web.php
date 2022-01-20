4a888c304c3b5c83e60f342be5c4d77e1b3938ca
mattiagiaco <martex781227@gmail.com>
2022-01-20 14:17:51 +0100

    complete

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    $data = [
+        'name' => 'Mattia'
+    ];
+
+    return view('home', $data);
+})->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/FAQ', function () {
+
+    $data = [
+        'faqs' => [
+            "Does Shift upgrade everything?",
+            "Does Shift do more than the Laravel Upgrade Guide?",
+            "Can I upgrade multiple versions at once?",
+            "What if my project is not on Bitbucket/GitHub/GitLab?",
+            "What if my boss/client/legal/paranoia won't let me share my project?"
+        ]
+    ];
+
+    return view('faq', $data);
+})->name('faq');
